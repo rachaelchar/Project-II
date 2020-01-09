30528a31a4949be4afef5c6653d1b61de936d2d1
RachaelChar <charewicz.r@gmail.com>
2020-01-09 11:56:21 -0500

    conform to eslint

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,52 +2,52 @@ const router = require('express').Router();
 const db = require('../models');
 
 router.get('/employees', (req, res) => {
-    db.Employee.findAll()
-        .then((employees) => {
-            res.json(employees);
-        });
+  db.Employee.findAll()
+    .then((employees) => {
+      res.json(employees);
+    });
 });
 
 router.get('/employees/:id', (req, res) => {
-    const id = req.params.id;
-    db.Employee.findOne({
-        where: {
-            id: id
-        }
-    })
+  const { id } = req.params;
+  db.Employee.findOne({
+    where: {
+      id,
+    },
+  })
     .then((employees) => {
-        res.json(employees);
+      res.json(employees);
     });
 });
 
 router.post('/employees', (req, res) => {
-    const employee = req.body;
-    db.Employee.create(employee)
-        .then((results) => {
-            res.json({
-                success: true
-            });
-        })
-        .catch((err) => {
-            console.log(err)
-            res.status(500).json({
-                success: false,
-                errors: err.errors
-            });
-        });
+  const employee = req.body;
+  db.Employee.create(employee)
+    .then((results) => {
+      res.json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        success: false,
+        errors: err.errors,
+      });
+    });
 });
 
 router.delete('/employees/:id', (req, res) => {
-    const id = req.params.id;
-    db.Employee.destroy({
-        where: {
-            id: id
-        }
-    }).then((response) => {
-        res.json({
-            success: true
-        });
+  const { id } = req.params;
+  db.Employee.destroy({
+    where: {
+      id,
+    },
+  }).then((response) => {
+    res.json({
+      success: true,
     });
+  });
 });
 
 module.exports = router;
