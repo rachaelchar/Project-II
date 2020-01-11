92d181b584cb0d3aca64f2b7c76c6062dc124fc0
RachaelChar <charewicz.r@gmail.com>
2020-01-11 10:39:37 -0500

    add html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,18 @@
 const path = require('path');
 const router = require('express').Router();
 
-router.get('/employees', (req, res) => {
+router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
+router.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/adminPage.html'));
+});
+
+router.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/profile.html'));
+});
+
 
 // Place this route below all others to send he index.html file
 // to any request that is not explicitly defined above
