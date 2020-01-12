98ff0e392cd7609bf29b438fdf452aa862d39626
bfall-debug <bfall@invengeon.com>
2020-01-11 23:33:32 -0500

    eslinted formatting errors

--- public/assets/js/picture.js
+++ public/assets/js/picture.js
@@ -1,79 +1,73 @@
-//============ Location of Pic Frame and Button ===============
+//= =========== Location of Pic Frame and Button ===============
 const pDiv = $('#picDiv');
 const btnDiv = $('#btnDiv');
-let width = 640;
-//=============================================================
+const width = 640;
+//= ============================================================
 
 let height = 0;
 let video = null;
-let canvas = null;
-let photo = null;
 
 const streaming = false;
 const cameraDiv = $('<div class="camera"><video id="video"></video><div class="overlay-desc"><h1 id="countdown"></h1></div></div>');
 const canvasDiv = $('<canvas id="canvas"></canvas><div class="output"><img id="photo"></div>');
-const webcam = $('<button id="webcam" class="camBtns">Start Webcam</button>')
-const snapshot = $('<button id="snapshot" class="camBtns">Take Picture</button>')
-const savePic = $('<button id="saveBtn" >Submit Picture</button>')
-
-$(document).on("click", '#webcam', () => {
-  btnDiv.empty().append(snapshot);
-  $('#snapshot').prop('disabled', false);
-  pDiv.empty().prepend(cameraDiv);
-  video = $('#video')[0]
-
-  navigator.mediaDevices.getUserMedia({ audio: false, video: true })
-    .then(mediaStream => {
-      video.srcObject = mediaStream;
-      video.play();
-      capture(mediaStream);
-    })
-
-  video.addEventListener('canplay', () => {
-    if (!streaming) {
-      height = video.videoHeight / (video.videoWidth / width);
-      $('#video').attr({ width: width, height: height });
-    }
-  });
-
-})
-
-$(document).on("click", '#saveBtn', () => {
-  alert("picture saved (not really)")
-})
+const webcam = $('<button id="webcam" class="camBtns">Start Webcam</button>');
+const snapshot = $('<button id="snapshot" class="camBtns">Take Picture</button>');
+const savePic = $('<button id="saveBtn" >Submit Picture</button>');
 
 function capture(mediaStream) {
-  $('#snapshot').on("click", () => {
+  $('#snapshot').on('click', () => {
     $('#snapshot').prop('disabled', true);
-    var timeleft = 6;
+    let timeleft = 6;
 
-    var timer = setInterval(() => {
+    const timer = setInterval(() => {
       timeleft -= 1;
       $('#countdown').text(timeleft);
       if (timeleft <= 0) {
         clearInterval(timer);
-        $('#countdown').text(" ")
+        $('#countdown').text(' ');
       }
     }, 1000);
 
     setTimeout(() => {
       pDiv.empty().prepend(canvasDiv);
-      $('#canvas').attr({ width: width, height: height });
+      $('#canvas').attr({ width, height });
       const context = $('#canvas')[0].getContext('2d');
 
       if (width && height) {
-        context.drawImage(video, 0, 0, width, height);
+        context.drawImage(video[0], 0, 0, width, height);
         const data = $('#canvas')[0].toDataURL('image/png');
         $('#photo').attr('src', data);
       }
 
-      const tracks = mediaStream.getTracks()
-      tracks[0].stop()
-      cameraDiv.remove()
-      btnDiv.empty().append(webcam).append(savePic)
+      const tracks = mediaStream.getTracks();
+      tracks[0].stop();
+      cameraDiv.remove();
+      btnDiv.empty().append(webcam).append(savePic);
     }, 6100);
-
-
-
   });
 }
+
+$(document).on('click', '#webcam', () => {
+  btnDiv.empty().append(snapshot);
+  $('#snapshot').prop('disabled', false);
+  pDiv.empty().prepend(cameraDiv);
+  video = $('#video');
+
+  navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+    .then((mediaStream) => {
+      video[0].srcObject = mediaStream;
+      video[0].play();
+      capture(mediaStream);
+    });
+
+  video[0].addEventListener('canplay', () => {
+    if (!streaming) {
+      height = video[0].videoHeight / (video[0].videoWidth / width);
+      $('#video').attr({ width, height });
+    }
+  });
+});
+
+$(document).on('click', '#saveBtn', () => {
+  console.log('picture saved (not really)');
+});
