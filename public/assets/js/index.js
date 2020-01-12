130abff02301c139e38dc2409afad7e5257d0633
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2020-01-12 14:42:56 -0500

    clock function is working, added moment.js file to run moment

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,19 +1,6 @@
-$("#new-employee-form").on("submit", function(event){
-    event.preventDefault();
-    const employee = {
-        first_name: $("#first_name").val(),
-        last_name: $("#last_name").val(),
-        admin: $("#admin").val(),
-        code: $("#code").val(),
-        working_status: $("#working_status").val(),
-        position: $("#position").val(),
-        hire_date: $("#hire_date").val(),
-        picture_fp: $("#picture_fp").val(),
-        pay_type: $("#pay_type").val(),
-    };
+setInterval(()=>{ 
+    var time = moment().format('MMMM Do YYYY, h:mm:ss ');
 
-    axios.post("/api/employees", employee)
-        .then((response) => {
-            window.location = "/employees";
-        });
-})
+    // console.log(time);
+    
+    $('#currentTime').html("<p>"+time+"</p>"); }, 1000);
