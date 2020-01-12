a9ce01865a294d0122c657754866393e2098aaf8
Aaron-R-Carter <aaronrobertcarter@gmail.com>
2020-01-12 15:49:56 -0500

    refactored and fixed clock and date

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,12 @@
-setInterval(()=>{ 
-    var time = moment().format('MMMM Do YYYY, h:mm:ss ');
 
-    // console.log(time);
-    
-    $('#currentTime').html("<p>"+time+"</p>"); }, 1000);
+printClock();
+
+
+// run clock function
+function printClock() {
+    setInterval(() => {
+        var time = moment().format('h:mm a');
+        var day = moment().format('dddd, MMMM Do');
+        $('#currentTime').html("<p class='text-center text-6xl font-bold text-gray-900 mb-1'>" + time + "</p>  <p class='text-center text-1xl font-bold text-gray-900 mb-6'>" + day + "</p>");
+    }, 1000);
+};
