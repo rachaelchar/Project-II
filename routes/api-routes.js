ed18072dc76bc6b1e60eb5d7d319a855ab311630
RachaelChar <charewicz.r@gmail.com>
2020-01-17 16:28:04 -0500

    commented out /api/employees as alternative for submit: revisit

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -34,6 +34,7 @@ router.put('/employees/clockin', (req,res) => {
 })
 
 router.post('/employees', (req, res) => {
+// router.post('/api/employees', (req, res) => {
   const employee = req.body;
   db.employee.create(employee)
     .then(() => {
