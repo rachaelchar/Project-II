c82ff78f52efa5030f5cab1096db5829c1252df9
bfall-debug <bfall@invengeon.com>
2020-01-18 00:46:27 -0500

    picture taking capabilities added

--- public/assets/js/picture.js
+++ public/assets/js/picture.js
@@ -1,3 +1,29 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("pic");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 //= =========== Location of Pic Frame and Button ===============
 const pDiv = $('#picDiv');
 const btnDiv = $('#btnDiv');
@@ -10,9 +36,9 @@ let video = null;
 const streaming = false;
 const cameraDiv = $('<div class="camera"><video id="video"></video><div class="overlay-desc"><h1 id="countdown"></h1></div></div>');
 const canvasDiv = $('<canvas id="canvas"></canvas><div class="output"><img id="photo"></div>');
-const webcam = $('<button id="webcam" class="camBtns">Start Webcam</button>');
-const snapshot = $('<button id="snapshot" class="camBtns">Take Picture</button>');
-const savePic = $('<button id="saveBtn" >Submit Picture</button>');
+const webcam = $('<button id="webcam" class="camBtns cursor-pointer bg-blue-600 hover:bg-blue-500 shadow-xl text-center px-5 py-2 inline-block text-blue-100 hover:text-white rounded">Start Webcam</button>');
+const snapshot = $('<button id="snapshot" class="camBtns cursor-pointer bg-blue-600 hover:bg-blue-500 shadow-xl text-center px-5 py-2 inline-block text-blue-100 hover:text-white rounded">Take Picture</button>');
+const savePic = $('<button id="saveBtn" class="cursor-pointer bg-blue-600 hover:bg-blue-500 shadow-xl text-center px-5 py-2 inline-block text-blue-100 hover:text-white rounded" >Submit Picture</button>');
 
 function capture(mediaStream) {
   $('#snapshot').on('click', () => {
