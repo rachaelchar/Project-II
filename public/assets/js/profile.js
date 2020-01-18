8410d00888b6d8edc7b89be56cf1979a4ec6aeb8
RachaelChar <charewicz.r@gmail.com>
2020-01-17 20:49:10 -0500

    console log employee that corresponds to clicked view button

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,7 +1,19 @@
-id = document.location.search.replace(/^.*?\=/,'');
+id = document.location.search.replace(/^.*?\=/, '');
 console.log(id)
 
 $(document).on('click', '#make-new-id-button', function (event) {
-    window.location = `/sampleBadge?id=${id}`;
-  });
-  
+  window.location = `/sampleBadge?id=${id}`;
+});
+
+const displayEmployeeData = function () {
+  axios.get(`/api/employees/?id=${id}`)
+    .then((response) => {
+      console.log("response", response.data);
+      // $('#employee-name') = `$('#employee-name').text()${response} 
+      
+
+      // $table.append(`<tr><td class="border px-4 py-2">${employee.first_name} ${employee.last_name}</td><td class="border px-4 py-2 ${clocked}" id="${employee.id}">${status}</td><td class="border px-4 py-2"><button data-id="${employee.id}" id="viewProfileBtn"class="justify-center bg-blue-500 hover:bg-gray-600 text-white font-bold py-2 px-4 border">View</button></td></tr>`);
+    });
+};
+
+displayEmployeeData();
